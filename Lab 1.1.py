7bc8028c79e4fd51526a0f1a1769f448f8ad22a2
WBingJ <90165990+WBingJ@users.noreply.github.com>
2021-09-06 10:47:52 +0800

    Add files via upload

---
+++ Lab 1.1.py
@@ -0,0 +1,10 @@
+# Student ID: 1201200237 #
+# Student Name: Wong Bing Jie #
+
+print("i like Python")
+
+person = input("Please enter your name: ")
+print("Hello ",person)
+
+# Lab 1.2 get input teo numbers and display the sum of the teo numbers
+

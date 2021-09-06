877e17cb587807cbe2a5e8738b4960ce14d786d3
WBingJ <90165990+WBingJ@users.noreply.github.com>
2021-09-06 10:49:37 +0800

    Add files via upload

---
+++ Lab 1.2.py
@@ -0,0 +1,13 @@
+# Lab 1.2 get input two numbers and display the sum of the two numbers
+
+num_1 = int(input("Enter your number 1 :"))
+num_2 = int(input("Enter your number 2 :"))
+
+total = num_1+num_2
+
+# print("The total number is ",total)
+
+print("The Sun is ",total," ",num_1,"and ",num_2)
+print("The sum of {} and {} is {}".format(num_1,num_2,total))
+print("number 1 is :",num_1)
+print("number 2 is :",num_2)
